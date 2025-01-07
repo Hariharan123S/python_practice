4bc0710e62d769dfe7e4044555948a779768580a
Hariharan123S <148625245+Hariharan123S@users.noreply.github.com>
2025-01-07 19:27:49 +0530

    Delete Beginner directory

--- Beginner/main.py
+++
@@ -1,9 +0,0 @@
-'''print("Welcome to the Band Name Generator.")
-ci = input("What's the name of the city you gre up in?\n")
-pt = input("What's your pet's name?\n")
-print("Your band name could be " + ci + " " + pt)'''
-
-#print("Number of letters in your name: " + str(len(input("Enter your name"))))
-name = input("Enter your name: ")
-lt = len(name)
-print("Number of letters in your name: " + str(lt))
